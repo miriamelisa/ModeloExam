448b5c136692d55d8eb78bbdb134d11da2546d4b
miriamelisa <miriam.guanolema1@gmail.com>
2023-10-31 20:05:33 +0100

    Creación de proyecto

---
+++ src/main/java/spring/modelo/ModeloExamApplication.java
@@ -0,0 +1,13 @@
+package spring.modelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/modelo/ModeloExamApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.modelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
